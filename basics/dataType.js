b687f79c898121ea1babb1d619ccc0c5117e995e
Sandeep Manjhi <138104501+manjhisandeep17@users.noreply.github.com>
2024-01-31 08:17:56 +0000

    push

---
+++ basics/dataType.js
@@ -0,0 +1,17 @@
+"use strict"; // treat as a new js
+
+// alert (3+3)  we are using node js not browsers
+
+// console.log(3 + 2);
+// console.log("Sand");
+
+// let Name = "Manjhi"
+// let age =24
+// let isLogedIn = false
+// let salary = null;
+// let state;
+
+// console.table([Name, age, isLogedIn, salary])
+
+// console.log(typeof salary);
+// console.log(typeof state);
---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234565
+let accountEmail= "sand@gmail.com"
+var accountPassword = "123456789"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = "123"  // not Allowed
+
+// console.log(accountId);
+
+accountEmail = "abc@gmail.com"
+console.table([accountEmail, accountPassword, accountCity, accountState]);
